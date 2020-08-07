1c075323ac0851fdd472393663b5055516f1246e
Bruno Gomes <bgpedrosas@gmail.com>
2020-08-06 23:18:15 -0300

    Finished Front and Backend application

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,32 +3,50 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css'
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+  avatar: string;
+  bio: string;
+  cost: number;
+  id: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+};
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps>= ({teacher}) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    })
+  }
+  
   return(
         <article className="teacher-item">
           <header>
-            <img src="https://avatars2.githubusercontent.com/u/67613857?s=460&u=0d6a5061aae6e43ce75e76d7d0288a7ad7b5d80f&v=4" alt="Bruno Pedrosa"/>
+            <img src={teacher.avatar} alt={teacher.name}/>
             <div>
-              <strong>Bruno Pedrosa</strong>
-              <span>Cálculo</span>
+              <strong>{teacher.name}</strong>
+              <span>{teacher.subject}</span>
             </div>
           </header>
 
-          <p>O matemático mais rápido do Sertão.
-            <br /><br />
-            Efetuou mais de 500 integrais em tempo recorde. Mais de 200 engenheiros alunos derrubaram suas construções.
-          </p>
+          <p>{teacher.bio}</p>
 
           <footer>
             <p>
               Preço/hora:
-              <strong>R$ 25,00</strong>
+              <strong>R$ {teacher.cost}</strong>
             </p>
-            <button type="button">
+            <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
               <img src={whatsappIcon} alt="Whatsapp"/>
               Entrar em contato
-            </button>
+            </a>
           </footer>
         </article>
   );
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { getDefaultSettings } from 'http2';
+// import { getDefaultSettings } from 'http2';
 
 ReactDOM.render(
   <React.StrictMode>
